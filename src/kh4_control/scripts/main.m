0233c702dad62673120d843a1513230c9bee6f38
Javier Leguina <javierleguina98@gmail.com>
2020-05-01 10:07:47 +0100

    Working simulink. Need to fix odometry/SLAM

--- src/kh4_control/scripts/main.m
+++ src/kh4_control/scripts/main.m
@@ -13,13 +13,23 @@ plot_flag = 0;
 %status = system('cd ~/catkin_ws && roslaunch kh4_gazebo kh4.launch','-echo');
 
 
-% parpool(2)
-parfor i = 1:2
+% parpool(3)
+parfor i = 1:3
     if i == 1
+        path = [3 3
+            -1 0];
         rosinit('http://localhost:11311');
         followTrajectory(path, '/robot1', plot_flag)
         rosshutdown;
-    else
+    elseif i == 2
+        path = [4 4
+            0 0];
+        rosinit('http://localhost:11311');
+        followTrajectory(path, '/robot2', plot_flag)
+        rosshutdown;
+    elseif i == 3
+        path = [5 5
+            1 0];
         rosinit('http://localhost:11311');
         followTrajectory(path, '/robot2', plot_flag)
         rosshutdown;
